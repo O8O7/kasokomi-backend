75a8c0436c611a915b74cf4036fb49f6666a6c8a
Sakamoto <firesamurai.jp@gmail.com>
2022-01-27 00:47:05 +0900

    static変更

---
+++ READORE.md
@@ -0,0 +1,4 @@
+deploy は https://www.youtube.com/watch?v=zizzeE4Obc0 を参考に heroku でデプロイした
+heroku のデプロイボタンで main ブランチをデプロイする
+log の見方は heroku login して
+heroku logs --app kasokomi -t
--- mysite/settings.py
+++ mysite/settings.py
@@ -140,13 +140,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-# STATIC_ROOT = BASE_DIR / 'staticfiles/'
-# STATIC_FILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_FILES_DIRS = [os.path.join(BASE_DIR / 'static')]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_FILES_DIRS = [os.path.join(BASE_DIR / 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
