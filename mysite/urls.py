b260b34b0a92d567216ee57e346d2b2d51c4ccc8
Sakamoto <firesamurai.jp@gmail.com>
2022-01-28 19:07:47 +0900

    S3設定

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,11 +15,11 @@ urlpatterns = [
 ]
 #  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# if settings.DEBUG:
-#     urlpatterns += static(
-#         settings.MEDIA_URL,
-#         document_root=settings.MEDIA_ROOT
-#     )
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
 # urlpatterns += static(
 #     settings.MEDIA_URL,
 #     document_root=settings.MEDIA_ROOT
@@ -28,9 +28,9 @@ urlpatterns = [
 # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG == False:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
-        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG == False:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
+#         static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # 開発用
 
