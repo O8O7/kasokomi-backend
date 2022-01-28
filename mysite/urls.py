673e593ea73dd6498a29931925cb08a4261db5d5
Sakamoto <firesamurai.jp@gmail.com>
2022-01-28 15:12:53 +0900

    static urlpattern変更

--- mysite/urls.py
+++ mysite/urls.py
@@ -28,9 +28,9 @@ if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
         static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG == False:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
-        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG == False:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
+#         static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # 開発用
 
